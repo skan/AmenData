d24f8ad34c7c4995bd5657275ce9770fff223ab0
U-ST\hamzas <hamzas@TUNCWL0102.tun.st.com>
2016-07-03 21:05:34 +0100

    organise 2015 data

---
+++ uploaded_qif/sk_script.sh
@@ -0,0 +1,24 @@
+echo " ***** 2015-01.qif *****" >> 2015.qif
+more 2015-01.qif >> 2015.qif
+echo " ***** 2015-02.qif *****" >> 2015.qif
+more 2015-02.qif >> 2015.qif
+echo " ***** 2015-03.qif *****" >> 2015.qif
+more 2015-03.qif >> 2015.qif
+echo " ***** 2015-04.qif *****" >> 2015.qif
+more 2015-04.qif >> 2015.qif
+echo " ***** 2015-05.qif *****" >> 2015.qif
+more 2015-05.qif >> 2015.qif
+echo " ***** 2015-06.qif *****" >> 2015.qif
+more 2015-06.qif >> 2015.qif
+echo " ***** 2015-07.qif *****" >> 2015.qif
+more 2015-07.qif >> 2015.qif
+echo " ***** 2015-08.qif *****" >> 2015.qif
+more 2015-08.qif >> 2015.qif
+echo " ***** 2015-09.qif *****" >> 2015.qif
+more 2015-09.qif >> 2015.qif
+echo " ***** 2015-10.qif *****" >> 2015.qif
+more 2015-10.qif >> 2015.qif
+echo " ***** 2015-11.qif *****" >> 2015.qif
+more 2015-11.qif >> 2015.qif
+echo " ***** 2015-12.qif *****" >> 2015.qif
+more 2015-12.qif >> 2015.qif
